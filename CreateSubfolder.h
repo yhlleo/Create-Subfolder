9fd055f4ad13962e72139c27f3f7af8c2142f215
yhlleo <yhlleo@users.noreply.github.com>
2016-01-27 21:49:56 +0800

    Create CreateSubfolder.h

---
+++ CreateSubfolder.h
@@ -0,0 +1,30 @@
+/* create subfolder for results saving.
+
+   - Editor: Yahui Liu.
+   - Data:   2016-01-27
+   - Email:  yahui.cvrs@gmail.com
+   - Address: Computer Vision and Remote Sensing(CVRS), Lab.
+ */
+
+#ifndef CREATE_SUBFOLDER_H
+#define CREATE_SUBFOLDER_H
+#pragma once
+#include <direct.h>
+#include <stdlib.h>  
+#include <stdio.h> 
+#include <string>
+
+using namespace std;
+
+class CreateSubfolder
+{
+public:
+	CreateSubfolder(){}
+	~CreateSubfolder(){}
+
+public:
+	static void creatSubfolderViaFname( 
+		char* filepath, char* subfolder, char* filename );
+};
+
+#endif // CREATE_SUBFOLDER_H
